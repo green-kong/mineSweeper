96479bc95e1915f71c1160309cb415d69a8bfea2
chacha912 <dkwkdnwk912@gmail.com>
2022-10-03 20:24:42 +0900

    Deploy Next.js app to Github Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  /**
+   * Tell Next.js where the `public` folder is.
+   */
+  assetPrefix: isProd ? '/mineSweeper/' : '',
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
