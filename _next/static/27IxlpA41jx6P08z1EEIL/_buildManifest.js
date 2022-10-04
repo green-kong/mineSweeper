3ea806f4b69baaf2f04e9848593a8b2dadb2cf90
green-kong <green-kong@users.noreply.github.com>
2022-10-04 07:15:56 +0000

    deploy: 8af5354058da7f51f7d2d3e2396bfe680e8db514

---
+++ _next/static/27IxlpA41jx6P08z1EEIL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-39259b1b14d9ab7b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
