5e13b6274653604ddc0633252641012395921619
dev_kong <ahdjd5@gmail.com>
2022-10-04 16:09:03 +0900

    feat: mark wrong flag when lose game

--- utils/checkMine.ts
+++ utils/checkMine.ts
@@ -1,10 +1,19 @@
-const checkMine = (game: any[][], open: boolean[][]): boolean => {
+const checkMine = (
+  game: any[][],
+  open: boolean[][]
+): false | [number, number][] => {
+  const bombList: [number, number][] = [];
+
   for (let i = 0; i < open.length; i++) {
     for (let j = 0; j < open[i].length; j++) {
       if (open[i][j] && game[i][j] === 'mine') {
-        return true;
+        bombList.push([i, j]);
       }
     }
+  }
+
+  if (bombList.length) {
+    return bombList;
   }
   return false;
 };
